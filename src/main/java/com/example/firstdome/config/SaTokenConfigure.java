875d6d3e11c5bf0a05cf00f029c0c7d2d5232b38
lw <3281403695@qq.com>
2025-09-26 16:38:08 +0800

    111

--- src/main/java/com/example/firstdome/config/SaTokenConfigure.java
+++ src/main/java/com/example/firstdome/config/SaTokenConfigure.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.annotation.PostConstruct;
@@ -42,6 +43,13 @@ public class SaTokenConfigure implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 将 http://localhost:8081/image/** 映射到 C:/file/image/
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:C:/file/image/");
     }
 //    /**
 //     * 重写Sa-Token框架内部算法策略
